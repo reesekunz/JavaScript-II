7f0cac89190cf56a30669866dd206bc3e1b9647a
Reese Kunz <reesekunz21@gmail.com>
2019-06-11 12:15:38 -0700

    halfway through callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,22 +27,25 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 // #1. firstItem passes the first item of the given array to the callback function.
 
 function firstItem(arr,cb)
-{
-return cb (arr[0]);
-}
+{ return cb (arr[0]); }
 
 firstItem(items, function(first)
-{console.log(first)});
+{ console.log(first) });
 
  // #2 getLength passes the length of the array into the callback.
 function getLength(arr, cb) 
-{
-return cb (getLength);
-}
+{ return cb (arr.length); }
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-}
+getLength(items,function(arrayLength)
+{ console.log(arrayLength) });
+
+// #3 last passes the last item of the array into the callback.
+function last(arr, cb) 
+{ return cb (arr[arr.length-1]) }
+
+last(items,function(lastitem)
+{ console.log(lastitem) });
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
