5d322b8fc3c806af897bb0543bd6d93a87ec02e6
Reese Kunz <reesekunz21@gmail.com>
2019-06-11 13:41:25 -0700

    finished closure section of project

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,25 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+var addTo = function(passed)
+
+{var add = function(inner)
+{return passed + inner; };
+
+return add; };
+
+var addFive = new addTo(5)
+var addEight = new addTo(8)
+var addTen = new addTo(10)
+
+console.dir(addFive);
+console.dir(addEight);
+console.dir(addTen);
+
+console.log(addFive(2));
+console.log(addEight(4));
+console.log(addTen(10));
+  
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
